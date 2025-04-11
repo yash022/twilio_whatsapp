30805e1dc771873516989ae6d41a822b1ecc9e2a
Yash Sehgal <96070479+yash022@users.noreply.github.com>
2025-04-11 15:45:46 +0530

    Update whatsapp.py

--- api/whatsapp.py
+++ api/whatsapp.py
@@ -1,6 +1,11 @@
 from twilio.rest import Client
 from flask import Flask 
 import os
+import logging
+
+logging.warning("SID: %s", os.environ.get("account_sid"))
+logging.warning("TOKEN: %s", os.environ.get("auth_token"))
+
 
 account_sid = os.environ['account_sid']
 auth_token = os.environ['auth_token']
